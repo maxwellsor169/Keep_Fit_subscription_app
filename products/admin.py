1b9d74f1bdf5b801008a61d5ba15c908c27d8c33
atm2chx3@students.codeinstitute.net <atm2chx3@students.codeinstitute.net>
2025-06-23 12:46:18 +0100

    Customize the admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,39 @@
 from django.contrib import admin
 from .models import Product, Category
 
+"""
+This Tells admin which field to display
+This Register Product model in admin panel
+"""
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+    search_fields = ['name', 'description', 'sku']
+    list_filter = ('category', 'status', 'price')
+
+
+"""
+This Register Category model in admin panel
+This Adds how category items are displaying.
+"""
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
