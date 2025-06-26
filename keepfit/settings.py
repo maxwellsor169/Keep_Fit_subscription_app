be4654d958e4d61a9ba22eb76a66252342c5cc1f
atm2chx3@students.codeinstitute.net <atm2chx3@students.codeinstitute.net>
2025-06-26 11:02:10 +0100

    Added opacity to homepage text with css styling

--- keepfit/settings.py
+++ keepfit/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
     'keep-fit-app-1a6153cb9738.herokuapp.com',  # heroku application
 ]
 
@@ -166,6 +168,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'keep-fit-app' 
     AWS_S3_REGION_NAME = 'eu-north-1'
