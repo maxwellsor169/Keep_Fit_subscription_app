fd4fa528489b86a47b8e0f059b86bfa2d035ab2e
atm2chx3@students.codeinstitute.net <atm2chx3@students.codeinstitute.net>
2025-07-07 08:34:06 +0100

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ri8wN2KEg0zizs07P7HC0WrGWubVenNoSulm1Kt8laBLjuVVV9rMGnXvsFLnqCX0py4SO5A3K8p39smQ2nK0QxY00bEkt6L3P',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
