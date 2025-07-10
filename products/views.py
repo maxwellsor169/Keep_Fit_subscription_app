cd7a64ac20d0e2c053939f33066cdab22f858032
atm2chx3@students.codeinstitute.net <atm2chx3@students.codeinstitute.net>
2025-07-11 00:49:16 +0100

    Product-admin - added product views, urls and template

--- products/views.py
+++ products/views.py
@@ -1,27 +1,21 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.db.models.functions import Lower
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 from .models import Product, Category
-
-
-"""
-This code is a combination of a student project 
-Fitness-subscription-app
-and The BouBoutique Ado project
-"""
+from .forms import ProductForm
 
 
 def all_products(request):
-    """ A view to show all products, including sorting queries """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
     categories = None
     sort = None
     direction = None
-       
+
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -36,7 +30,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-                      
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -50,7 +44,7 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-            
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
@@ -73,3 +67,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
