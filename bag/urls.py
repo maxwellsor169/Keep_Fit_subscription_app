93c99fa06d524e86e586654edf35fa9c71aebc20
atm2chx3@students.codeinstitute.net <atm2chx3@students.codeinstitute.net>
2025-07-10 12:40:47 +0100

    Updated allauth template

--- bag/urls.py
+++ bag/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('add/product/<item_id>/', views.add_to_bag, name='add_to_bag'),
     path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
-    path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
+    path(
+        'remove/product/<item_id>/',
+        views.remove_from_bag,
+        name='remove_from_bag'
+    ),
 ]
