81fbce9c723c9783cad407c969e53da13dd6e682
atm2chx3@students.codeinstitute.net <atm2chx3@students.codeinstitute.net>
2025-07-11 21:17:42 +0100

    Added views, url and html template for plans app

--- plans/views.py
+++ plans/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Subscribe
 
-# Create your views here.
+# Used code from Boutique Ado
+
+
+def all_plans(request):
+    """ A view to show all products, including sorting queries """
+
+    plans = Subscribe.objects.all()
+
+    context = {
+        'plans': plans,
+    }
+
+    return render(request, 'plans/plans.html', context)
