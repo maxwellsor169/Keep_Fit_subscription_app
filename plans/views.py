214de695279ab11aa569a95e7342fab508e1a885
atm2chx3@students.codeinstitute.net <atm2chx3@students.codeinstitute.net>
2025-07-13 00:42:36 +0100

    Added final content for the plans app

--- plans/views.py
+++ plans/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.db.models import Q
+
 from .models import Subscribe
+
 
 # Used code from Boutique Ado
 
@@ -11,11 +15,57 @@ def all_plans(request):
     plans = Subscribe.objects.all()
     nutrition_plans = Subscribe.objects.filter(category='nutrition')
     exercise_plans = Subscribe.objects.filter(category='exercise')
+    sort = None
+    direction = None
+    selected_levels = []
+    selected_durations = []
+
+    if request.GET:
+        if "sort" in request.GET:
+            sortkey = request.GET["sort"]
+            sort = sortkey
+            if "direction" in request.GET:
+                direction = request.GET["direction"]
+                if direction == "desc":
+                    sortkey = f"-{sortkey}"
+            nutrition_plans = nutrition_plans.order_by(sortkey)
+            exercise_plans = exercise_plans.order_by(sortkey)
+
+    # Filtering by duration
+        if "duration_weeks" in request.GET and request.GET["duration_weeks"]:
+            duration = int(request.GET["duration_weeks"])
+            nutrition_plans = nutrition_plans.filter(duration_weeks=duration)
+            exercise_plans = exercise_plans.filter(duration_weeks=duration)
+            selected_durations = [duration]
+        else:
+            selected_durations = []  
+
+        # Filtering by level
+        if "level" in request.GET and request.GET["level"]:
+            level = int(request.GET["level"])
+            nutrition_plans = nutrition_plans.filter(level=level)
+            exercise_plans = exercise_plans.filter(level=level)
+            selected_levels = [level]
+        else:
+            selected_levels = []         
+
+    current_sorting = f'{sort or "None"}_{direction or "None"}'
+
+    all_durations = Subscribe.objects.values_list(
+        "duration_weeks", flat=True
+    ).distinct()
+
+    all_levels = Subscribe._meta.get_field("level").choices             
 
     context = {
         'plans': plans,
         'nutrition_plans': nutrition_plans,
         'exercise_plans': exercise_plans,
+        "current_sorting": current_sorting,
+        "selected_levels": selected_levels,
+        "selected_durations": selected_durations,
+        "all_durations": all_durations,
+        "all_levels": all_levels,
     }
 
     return render(request, 'plans/plans.html', context)
@@ -31,3 +81,4 @@ def plan_detail(request, plan_id):
     }
 
     return render(request, 'plans/plan_detail.html', context)
+
